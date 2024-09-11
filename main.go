e3e7745e8ecc17e9cb87e6c6888f1a3eff436942
ggarber42 <ggarber@protonmail.com>
2024-09-10 21:41:34 -0300

    feat: new handlers

--- main.go
+++ main.go
@@ -6,12 +6,36 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello app"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+
+	if r.Method != "POST"{
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("method not allowed"))
+		return	
+	}
+
+	w.Write([]byte("Create new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Printf("Start server on :4000")
 	err := http.ListenAndServe(":4000", mux)
